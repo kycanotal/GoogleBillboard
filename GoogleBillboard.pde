201cf684f5e8bae2856e89703e6f8f7b5cda6149
kycanotal <kycanotal@s.sfusd.edu>
2019-01-15 10:32:40 -0800

    fixed loop and removed decimal place

--- GoogleBillboard.pde
+++ GoogleBillboard.pde
@@ -1,12 +1,13 @@
-public final static String e = "2.718281828459045235360287471352662497757247093699959574966967627724076630353547594571382178525166427427466391932003059";  
+public final static String e = "2718281828459045235360287471352662497757247093699959574966967627724076630353547594571382178525166427427466391932003059";  
 public void setup()  
 {            
     for (int scan = 0; scan < e.length()-1; scan++)
      {
-     	double truth = Double.parseDouble(e.substring(scan, scan+9));
+     	double truth = Double.parseDouble(e.substring(scan, scan+10));
      		if (isPrime(truth) == true)
      		{
      			System.out.println(truth);
+     			break;
      		}
      }
    //your code here 
@@ -20,7 +21,7 @@ public boolean isPrime(double decimals)
     if(decimals < 2)
     return false;
     for(int i = 2; i <= Math.sqrt(decimals); i++)
-    if(decimals % == 0)
+    if(decimals % i == 0)
     return false;//your code here   
     return true;  
 } 
--- build-tmp/source/GoogleBillboard.java
+++ build-tmp/source/GoogleBillboard.java
@@ -12,3 +12,43 @@ import java.io.InputStream;
 import java.io.OutputStream; 
 import java.io.IOException; 
 
+public class GoogleBillboard extends PApplet {
+
+public final static String e = "2718281828459045235360287471352662497757247093699959574966967627724076630353547594571382178525166427427466391932003059";  
+public void setup()  
+{            
+    for (int scan = 0; scan < e.length()-1; scan++)
+     {
+     	double truth = Double.parseDouble(e.substring(scan, scan+10));
+     		if (isPrime(truth) == true)
+     		{
+     			System.out.println(truth);
+     			break;
+     		}
+     }
+   //your code here 
+}  
+public void draw()  
+{   
+	//not needed for this assignment
+}  
+public boolean isPrime(double decimals)  
+{   
+    if(decimals < 2)
+    return false;
+    for(int i = 2; i <= Math.sqrt(decimals); i++)
+    if(decimals % i == 0)
+    return false;//your code here   
+    return true;  
+} 
+
+
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "GoogleBillboard" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
